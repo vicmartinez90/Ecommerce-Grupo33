7b23afc7bef961c84394fee399991c2040618ae6
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-02 11:26:52 -0500

    server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a637f46650faa45b716da072ebcb83d'),
+    },
+  },
+});
