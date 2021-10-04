2423659103a0322162615f19014abe27cfc053a9
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-04 14:51:50 -0500

    Obteniedo todas las categorias

--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -31,12 +31,21 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
 import { getTokenApi, deleteTokenApi } from '../api/token';
+import { getCategoriesApi } from '../api/category';
 
 export default {
   name: 'menu',
 
   setup() {
+    let categories = ref(null);
+
+    onMounted(async () => {
+      const response = await getCategoriesApi();
+      categories.value = response;
+    });
+
     const token = getTokenApi();
 
     const logout = () => {
@@ -47,6 +56,7 @@ export default {
     return {
       token,
       logout,
+      categories,
     };
   },
 };
