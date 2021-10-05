19976c7c480b3cc0a6a2796f8792afd403064e63
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-05 14:37:52 -0500

    Obteniedo porductos de una categoria

--- client/src/views/Category.vue
+++ client/src/views/Category.vue
@@ -5,11 +5,24 @@
 </template>
 
 <script>
+import { onMounted } from 'vue';
 import BasicLayouts from '../layouts/BasicLayouts.vue';
+import { getCategoriesApi } from '../api/category';
 export default {
   name: 'Category',
   components: {
     BasicLayouts,
+  },
+  watch: {
+    $router(to, from) {
+      console.log(to);
+    },
+  },
+  setup() {
+    onMounted(async () => {
+      const response = await getCategoriesApi('fitness');
+      console.log(response);
+    });
   },
 };
 </script>
