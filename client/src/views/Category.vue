4e3a6e75ca73f67e8e59d982e2b025df41bc1def
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-05 15:00:18 -0500

    Obtenido productos al cambiar de categoria

--- client/src/views/Category.vue
+++ client/src/views/Category.vue
@@ -1,28 +1,44 @@
 <template>
   <BasicLayouts>
     <h2>Estamos en la categoria</h2>
+    {{ products }}
   </BasicLayouts>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import BasicLayouts from '../layouts/BasicLayouts.vue';
-import { getCategoriesApi } from '../api/category';
+import { getCategoriesApi, getProductsCategory } from '../api/products';
+
 export default {
   name: 'Category',
   components: {
     BasicLayouts,
   },
   watch: {
-    $router(to, from) {
-      console.log(to);
+    $route(to, from) {
+      this.getProduts(to.params.category);
     },
   },
+
   setup() {
-    onMounted(async () => {
-      const response = await getCategoriesApi('fitness');
-      console.log(response);
+    let products = ref(null);
+    const { params } = useRoute();
+
+    onMounted(() => {
+      getProduts(params.category);
     });
+
+    const getProduts = async (category) => {
+      const response = await getProductsCategory(category);
+      products.value = response;
+    };
+
+    return {
+      getProduts,
+      products,
+    };
   },
 };
 </script>
