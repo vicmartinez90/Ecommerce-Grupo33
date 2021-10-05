ad3e2acd3f246b4deb118be07c260be2935039a5
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-05 15:40:35 -0500

    Añadir productos al carrito  y acumular

--- client/src/components/Product.vue
+++ client/src/components/Product.vue
@@ -15,16 +15,21 @@
 
 <script>
 import { API_URL } from '../utils/constants';
+import { addProductCartApi } from '../api/cart';
+
 export default {
   name: 'Product',
   props: {
     product: Object,
   },
   setup(props) {
-    console.log(props.product);
+    const addProductCart = (idProduct) => {
+      addProductCartApi(idProduct);
+    };
 
     return {
       API_URL,
+      addProductCart,
     };
   },
 };
