e03ab3dddc704df317f546006b018908945d7192
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-06 14:32:58 -0500

    Creando carrito, abriendolo y cerrandolo

---
+++ client/src/components/Cart/Cart.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="cart-dimmer" :class="{ open: showCart }" @click="closeCart" />
+  <div class="cart" :class="{ open: showCart }">
+    <h2>Esto es el carrito</h2>
+    <button @click="closeCart">Cerrar</button>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'Cart',
+
+  setup() {
+    const store = useStore();
+    const showCart = computed(() => store.state.showCart);
+
+    const closeCart = () => {
+      store.commit('setShowCart', false);
+    };
+
+    return {
+      showCart,
+      closeCart,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cart-dimmer {
+  opacity: 0;
+  transition: opacity 0.5 ease;
+  &.open {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    opacity: 0.7;
+  }
+}
+
+.cart {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 400px;
+  height: 100vh;
+  background-color: #fff;
+  box-shadow: 0px 0px 26px 5px rgba(0, 0, 0, 0.75);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: transform 0.9s ease;
+  transform: translateX(150%);
+  &.open {
+    transform: translateX(0);
+  }
+}
+</style>
--- client/src/layouts/BasicLayouts.vue
+++ client/src/layouts/BasicLayouts.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="basic-layout">
-      <Menu />
-      <div class="ui container">
-          <slot />
-      </div>
+    <Menu />
+    <div class="ui container">
+      <slot />
+    </div>
   </div>
+
+  <Cart />
 </template>
 
 <script>
-import Menu from '../components/Menu.vue'
+import Menu from '../components/Menu.vue';
+import Cart from '../components/Cart/Cart.vue';
+
 export default {
-    name: 'BasicLayouts',
-    components: {
-        Menu,
-    },
-}
+  name: 'BasicLayouts',
+  components: {
+    Menu,
+    Cart,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
