6572db5bdbab65f114d212f33f9328c81babc04d
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-06 14:37:44 -0500

    Header del carrito

--- client/src/components/Cart/Cart.vue
+++ client/src/components/Cart/Cart.vue
@@ -2,16 +2,23 @@
   <div class="cart-dimmer" :class="{ open: showCart }" @click="closeCart" />
   <div class="cart" :class="{ open: showCart }">
     <h2>Esto es el carrito</h2>
-    <button @click="closeCart">Cerrar</button>
+    <div>
+      <CartHeader :closeCart="CartHeader" />
+    </div>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
+import CartHeader from './CartHeader.vue';
 
 export default {
   name: 'Cart',
+
+  components: {
+    CartHeader,
+  },
 
   setup() {
     const store = useStore();
